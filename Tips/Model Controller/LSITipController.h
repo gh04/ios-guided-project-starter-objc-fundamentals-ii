b59572771dacc4996ff0a6ee66331dcd5fb3565e
Gerardo <gahernandez02@gmail.com>
2020-07-23 19:27:49 -0700

    Added a private interface and mutable (var) array as an instance variable. Variable carried around.

--- Tips/Model Controller/LSITipController.h
+++ Tips/Model Controller/LSITipController.h
@@ -9,13 +9,22 @@
 #import <Foundation/Foundation.h>
 //instead of importing files in headers. Which you don't want to.
 //Won't stop you from making a misktake
+//the less we import, the faster compile times.
+// everything is accessible in obj c. You can see all method names.
+//if you need to make it 'private' use swift and it will dissappear at run time
 @class LSITip;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITipController : NSObject
-
+//helper methods to not make mutliple copies when you dont want to
 @property (nonatomic, readonly, copy) NSArray<LSITip *> *tips;
+
+@property (nonatomic, readonly) NSUInteger tipCount;
+
+- (LSITip *)tipAtIndex:(NSUInteger)index;
+
+- (void)addTip:(LSITip *)aTip;
 
 @end
 

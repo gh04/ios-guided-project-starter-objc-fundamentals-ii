9de322de64bdc8f498a16c2999f0069de5aa3660
Gerardo <gahernandez02@gmail.com>
2020-07-23 19:52:34 -0700

    Included LSITip and added a method that test test data

--- Tips/Model Controller/LSITipController.m
+++ Tips/Model Controller/LSITipController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LSITipController.h"
+#import "LSITip.h"
 //our own instance variables
 
 @interface LSITipController () {
@@ -21,8 +22,22 @@
 {
     if (self =[super init]) {
         _internalTips = [[NSMutableArray alloc] init];
+        [self addTestData];
     }
     return self;
+}
+
+- (void)addTestData
+{
+    LSITip *aTip = [[LSITip alloc] initWithName:@"Shushi"
+                                          total:200.
+                                     splitCount:4
+                                  tipPercentage:20.];
+    
+    [_internalTips addObject:aTip];
+    //alloc a tip so we can access. : our argument
+    [_internalTips addObject:[[LSITip alloc] initWithName:@"Pizza"
+                                                    total:30. splitCount:8 tipPercentage:25.]];
 }
 //when something is readyonly it becomes a method
 //implementing the getter. getting rid of that instance property for you.

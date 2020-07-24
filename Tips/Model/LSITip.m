613350fcb8faad5bc8c96efa3d639ab6dc008595
Gerardo <gahernandez02@gmail.com>
2020-07-23 19:09:25 -0700

    defined the initializer so LSItype

--- Tips/Model/LSITip.m
+++ Tips/Model/LSITip.m
@@ -10,4 +10,19 @@
 
 @implementation LSITip
 
+    -(instancetype)initWithName:(NSString *)aName
+total:(double)aTotal splitCount:(NSInteger)aSplitCount tipPercentage:(double)aTipPercentage;
+{
+
+if (self = [super init]) {
+    //instance variable direct access to
+    //accessing the underlying variable because we cannot set in readonly. 
+    _name = aName;
+    _total = aTotal;
+    _splitCount = aSplitCount;
+    
+    }
+    return self;
+}
+    
 @end
